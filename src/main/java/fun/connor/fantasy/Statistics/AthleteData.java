5b16cccf6ad0d781659fbb15b3d22ff116f81633
Aaron Aaeng <aaaa9270@colorado.edu>
2018-10-16 11:02:56 -0600

    Probably not using this.  Just saving

---
+++ src/main/java/fun/connor/fantasy/Statistics/AthleteData.java
@@ -0,0 +1,4 @@
+package fun.connor.fantasy.Statistics;
+
+public class AthleteData {
+}
---
+++ src/main/java/fun/connor/fantasy/Statistics/StatisticsTemplate.java
@@ -0,0 +1,8 @@
+package fun.connor.fantasy.Statistics;
+
+public abstract class StatisticsTemplate implements AthleteValuation {
+    public final Double getAthleteValue(AthleteData athleteData)
+    {
+        return 0.0;
+    }
+}
