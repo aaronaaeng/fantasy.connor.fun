b9f839315f72750b33602c74966ce1f5a7820526
Aaron Aaeng <aaaa9270@colorado.edu>
2018-10-30 20:33:46 -0600

    uncommented

--- src/main/java/fun/connor/fantasy/Database/DatabaseAccessObject.java
+++ src/main/java/fun/connor/fantasy/Database/DatabaseAccessObject.java
@@ -1,32 +1,31 @@
 package fun.connor.fantasy.Database;
 
 import com.google.gson.Gson;
-//import com.mongodb.MongoClient;
+import com.mongodb.MongoClient;
 import fun.connor.fantasy.Athlete.Athlete;
-import fun.connor.fantasy.Statistics.BowlerStatistics;
-//import org.mongodb.morphia.Datastore;
-//import org.mongodb.morphia.Morphia;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
 
 import java.util.UUID;
 
 public class DatabaseAccessObject {
-//    private final Morphia morphia = new Morphia();
+    private final Morphia morphia = new Morphia();
     private final Gson gson = new Gson();
-//    private final Datastore dataStore = morphia.createDatastore(new MongoClient(), "fantasy_database");
+    private final Datastore dataStore = morphia.createDatastore(new MongoClient(), "fantasy_database");
 
     public DatabaseAccessObject()
     {
-//        morphia.mapPackage("fun.connor.fantasy");
-//        dataStore.ensureIndexes();
+        morphia.mapPackage("fun.connor.fantasy");
+        dataStore.ensureIndexes();
     }
 
     public Athlete loadAthlete(UUID athleteID)
     {
-//        Athlete athlete = gson.fromJson("test", Athlete.class);
+        Athlete athlete = gson.fromJson("test", Athlete.class);
         return null;
     }
 
     public void saveAthlete(Athlete athlete) {
-//    };dataStore.save(athlete);
+        dataStore.save(athlete);
     }
 }
