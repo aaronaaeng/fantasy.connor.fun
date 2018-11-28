92291c9dfbf60568bbf423592a74cfaf56ce1c48
Aaron Aaeng <aaaa9270@colorado.edu>
2018-11-27 20:23:14 -0700

    Added docs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"fun.connor.fantasy"},{"l":"fun.connor.fantasy.Athlete"},{"l":"fun.connor.fantasy.Auth"},{"l":"fun.connor.fantasy.Budget"},{"l":"fun.connor.fantasy.Database"},{"l":"fun.connor.fantasy.Endpoints"},{"l":"fun.connor.fantasy.League"},{"l":"fun.connor.fantasy.Statistics"},{"l":"fun.connor.fantasy.Team"},{"l":"fun.connor.fantasy.Users"}]
